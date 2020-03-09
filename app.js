c5e53e7c20d5b258ffdeddec6de620e7f09cedb0
José Manuel <meloth96@gmail.com>
2020-03-08 23:21:20 -0600

    Added body-parser middleware.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const studentRoutes = require('./api/routes/students');
 
+app.use(bodyParser.json());
 app.use('/students', studentRoutes);
 
 module.exports = app;
