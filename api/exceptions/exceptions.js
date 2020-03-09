59f09a10fb772ffdf84892cc1a28959f2aee8928
José Manuel <meloth96@gmail.com>
2020-03-08 20:36:09 -0600

    Added missing call to super constructor in derived class.

--- api/exceptions/exceptions.js
+++ api/exceptions/exceptions.js
@@ -1,12 +1,14 @@
 module.exports = {
   StudentDAOException: class StudentDAOException extends Error{
     constructor(message){
+      super(message);
       this.message = message;
     }
   },
 
   ConnectionException: class ConnectionException extends Error{
     constructor(message){
+      super(message);
       this.message = message;
     }
   }
