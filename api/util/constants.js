63dfdf922396a99dd892a6793bfd48a78c23114b
José Manuel <meloth96@gmail.com>
2020-03-09 01:48:14 -0600

    Added ERROR_CODES Constants for a better and more specific exception handling.

---
+++ api/util/constants.js
@@ -0,0 +1,11 @@
+const Constants = {
+  ERROR_CODES: {
+    RESOURCE_NOT_FOUND: 0,
+    INVALID_PARAMETERS: 1,
+    CONNECTION_ERROR: 2,
+    QUERY_EXECUTION_ERROR: 3,
+    UNEXPECTED_ERROR: 4
+  }
+}
+
+module.exports = Constants;
